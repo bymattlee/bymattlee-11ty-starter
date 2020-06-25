51702b415151fd3e153594ccdb4cccf828f3e765
bymattlee <fivep0int9@gmail.com>
2020-06-25 14:24:27 -0700

    Clean up meta markup

--- src/site/_data/global.js
+++ src/site/_data/global.js
@@ -1,5 +1,8 @@
 module.exports = {
+	env: process.env.NODE_ENV,
   siteName: 'ByMattLee',
+  siteDescription: 'ByMattLee Web Starter Files',
+  siteShareImage: '/assets/images/share.jpg',
   twitterHandle: '@ByMattLee',
   googleAnalyticsId: ''
 }
