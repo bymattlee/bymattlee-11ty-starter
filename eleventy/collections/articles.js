d80860de7660d5c2210e8386df95b81400c345d8
bymattlee <fivep0int9@gmail.com>
2020-06-26 12:04:02 -0700

    Fix tag and pagination titles, add article detail pagination

--- eleventy/collections/articles.js
+++ eleventy/collections/articles.js
@@ -1,3 +1,14 @@
 module.exports = collection => {
-	return collection.getFilteredByGlob('./src/site/articles/*.md').reverse();
+	const articles = collection.getFilteredByGlob('./src/site/articles/*.md').reverse();
+
+  // Include previous and next article objects for detail pagination
+  for (let i = 0; i < articles.length; i++) {
+    const previousArticle = articles[i - 1];
+    const nextArticle = articles[i + 1];
+
+    articles[i].data['previousArticle'] = previousArticle;
+    articles[i].data['nextArticle'] = nextArticle;
+  }
+
+  return articles;
 }
